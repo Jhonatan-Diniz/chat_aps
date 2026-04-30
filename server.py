c97914c02ebbdf2c76825f00b54896f74d17c251
Jhonatan-Diniz <jhonatandiniz360@gmail.com>
2026-04-30 18:22:43 -0300

    Excluindo pasta de client deste repositorio Alterando funcoes para melhorar os testes

--- server.py
+++ server.py
@@ -16,71 +16,25 @@ class Team:
         return 1 if team.name == self.name and team.password == self.password else 0
 
 
+teams = []
+
+
 conn = sqlite3.connect("equipes.db")
 cur = conn.cursor()
 
 
-def send_msg(sender: int, receiver : int, msg_type, msg : bytes):
-   data =  msg_type + sender.to_bytes(1, "big") + len(msg).to_bytes(8, "big") + msg
-
-   for team in teams:
-       print(team.id_team)
-       print(receiver)
-       if receiver == team.id_team:
-           team_recv = get_team(receiver)
-           if team_recv is None : return
-           try:
-               team_recv.client.sendall(data)
-           except:
-               teams.remove(team)
-
-
-teams = []
-
-
-def recv_data(client, size) -> bytes:
-    data = b""
-    while len(data) < size:
-        pack = client.recv(size - len(data))
-        if pack is None:
-            break
-        data += pack
-    return data
-
-
-def handle_teams(team):
-    while True: 
-        try:
-            msg_type : bytes = recv_data(team.client, 1)
-            if msg_type is None : break
-            msg_receiver : bytes = recv_data(team.client, 1)
-            if msg_receiver is None : break
-            msg_size : bytes = recv_data(team.client, 8)
-            size : int = int.from_bytes(msg_size, "big")
-            msg : bytes = recv_data(team.client, size)
-
-            msg_recv = int.from_bytes(msg_receiver, "big")
-
-            send_msg(team.id_team, msg_recv, msg_type, msg)
-        except:
-            teams.remove(team)
-            team.client.close()
-            break
-
-
-def team_exist(team) -> bool | int:
-    cur.execute(f"SELECT id FROM equipes WHERE name='{team.name}' AND password='{team.password}'")
-    res = cur.fetchone()[0]
-    print(res)
-
-    if res: return res
-    return False
-
-
-def get_team(id) -> Team | None:
-    for team in teams:
-        if team.id_team == id:
-            return team
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        print("Starting server...")
+        soc.bind((HOST, PORT))
+        soc.listen()
+        while True:
+            client, addr = soc.accept()
+            team = login_team(client)
+            teams.append(team)
+            thread = threading.Thread(target=handle_teams, args=(team,))
+            thread.start() 
+            print(f"Client {addr} connected...")
 
 
 def login_team(client) -> Team:
@@ -105,20 +59,78 @@ def login_team(client) -> Team:
         else:
             client.sendall(b"F")
     return team
+ 
+
+def handle_teams(team):
+    while True: 
+        try:
+            msg_type : bytes = recv_data(team.client, 1)
+            msg_receiver : bytes = recv_data(team.client, 1)
+            if msg_type == b"G":
+                msg_recv_id = int.from_bytes(msg_receiver, "big")
+                get_team_name_by_id(msg_recv_id, team.soc)
+                continue
+
+            msg_size : bytes = recv_data(team.client, 8)
+            size : int = int.from_bytes(msg_size, "big")
+            msg : bytes = recv_data(team.client, size)
+            msg_recv_id = int.from_bytes(msg_receiver, "big")
+
+            send_msg(team.id_team, msg_recv_id, msg_type, msg)
+        except:
+            teams.remove(team)
+            team.client.close()
+            break
 
 
-def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        print("Starting server...")
-        soc.bind((HOST, PORT))
-        soc.listen()
-        while True:
-            client, addr = soc.accept()
-            team = login_team(client)
-            teams.append(team)
-            thread = threading.Thread(target=handle_teams, args=(team,))
-            thread.start() 
-            print(f"Client {addr} connected...") 
+def send_msg(sender: int, receiver : int, msg_type, msg : bytes):
+   data =  msg_type + sender.to_bytes(1, "big") + len(msg).to_bytes(8, "big") + msg
+
+   for team in teams:
+       if receiver == team.id_team:
+           team_recv = get_team(receiver)
+           if team_recv is None : return
+           try:
+               team_recv.client.sendall(data)
+           except:
+               teams.remove(team)
+
+
+def recv_data(client, size) -> bytes:
+    data = b""
+    while len(data) < size:
+        pack = client.recv(size - len(data))
+        if pack is None:
+            break
+        data += pack
+    return data
+
+
+def get_team_name_by_id(id, req):
+    team_name : bytes = b""
+    for team in teams:
+        if team.id == id:
+            team_name : bytes = team.name.encode("utf-8")
+    data : bytes = \
+    (len(team_name).to_bytes(1, "big") + team_name)
+    req.sendall(data)
+
+
+def team_exist(team) -> bool | int:
+    cur.execute(f"SELECT id FROM equipes WHERE name='{team.name}' AND password='{team.password}'")
+    res = cur.fetchone()
+    if not res: return False
+
+    team = res[0]
+    print(team)
+    if team: return team
+    return False
+
+
+def get_team(id) -> Team | None:
+    for team in teams:
+        if team.id_team == id:
+            return team
 
 
 if __name__ == "__main__":
